0b7fa2eb8228522851a03c24c1a768aadb749a5f
chomookun <chomookun@chomookuns-MacBook-Air.local>
2022-05-23 01:12:46 +0900

    .

--- apps-core/src/main/java/org/oopscraft/apps/core/CoreConfiguration.java
+++ apps-core/src/main/java/org/oopscraft/apps/core/CoreConfiguration.java
@@ -1,12 +1,19 @@
 package org.oopscraft.apps.core;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @ComponentScan
 @EnableJpaRepositories
-public class CoreConfiguration {
+public class CoreConfiguration implements EnvironmentPostProcessor {
 
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+
+    }
 }
--- apps-web/src/main/java/org/oopscraft/apps/web/WebConfiguration.java
+++ apps-web/src/main/java/org/oopscraft/apps/web/WebConfiguration.java
@@ -1,15 +1,22 @@
 package org.oopscraft.apps.web;
 
 import org.oopscraft.apps.core.CoreConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAutoConfiguration
 @ComponentScan
 @Import({CoreConfiguration.class})
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration implements EnvironmentPostProcessor, WebMvcConfigurer {
 
 
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+
+    }
 }
