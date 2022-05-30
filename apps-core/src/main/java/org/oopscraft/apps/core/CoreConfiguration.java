85a77eebbee1165ab1c64f8b97de2365081a0c17
chomookun <chomookun@gmail.com>
2022-05-31 02:50:50 +0900

    test

--- apps-core/src/main/java/org/oopscraft/apps/core/CoreConfiguration.java
+++ apps-core/src/main/java/org/oopscraft/apps/core/CoreConfiguration.java
@@ -1,19 +1,61 @@
 package org.oopscraft.apps.core;
 
+import lombok.Builder;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Properties;
 
 @EnableAutoConfiguration
 @ComponentScan
 @EnableJpaRepositories
 public class CoreConfiguration implements EnvironmentPostProcessor {
 
+    /**
+     * postProcessEnvironment
+     * @param environment the environment to post-process
+     * @param application the application to which the environment belongs
+     */
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-
+        Arrays.stream(environment.getActiveProfiles())
+                .sorted(Comparator.reverseOrder())
+                .forEach(profile -> {
+                    String location = String.format("classpath:core-config-%s.yml", profile);
+                    environment.getPropertySources().addLast(parseYamlResource(location));
+                });
+        environment.getPropertySources().addLast(parseYamlResource(("classpath:core-config.yml")));
     }
+
+    /**
+     * parseYamResource
+     * @param location
+     * @return
+     */
+    private PropertySource parseYamlResource(String location) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource(location);
+        if(resource.exists()) {
+            YamlPropertiesFactoryBean yamlPropertiesFactory = new YamlPropertiesFactoryBean();
+            yamlPropertiesFactory.setResources(resource);
+            yamlPropertiesFactory.afterPropertiesSet();
+            return new PropertiesPropertySource(location, yamlPropertiesFactory.getObject());
+        }else{
+            return new PropertiesPropertySource(location, new Properties());
+        }
+    }
+
 }
--- apps-web/src/main/java/org/oopscraft/apps/web/WebConfiguration.java
+++ apps-web/src/main/java/org/oopscraft/apps/web/WebConfiguration.java
@@ -1,22 +1,61 @@
 package org.oopscraft.apps.web;
 
 import org.oopscraft.apps.core.CoreConfiguration;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Properties;
 
 @EnableAutoConfiguration
 @ComponentScan
 @Import({CoreConfiguration.class})
-public class WebConfiguration implements EnvironmentPostProcessor, WebMvcConfigurer {
+public class WebConfiguration implements EnvironmentPostProcessor {
 
-
+    /**
+     * postProcessEnvironment
+     * @param environment the environment to post-process
+     * @param application the application to which the environment belongs
+     */
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-
+        Arrays.stream(environment.getActiveProfiles())
+                .sorted(Comparator.reverseOrder())
+                .forEach(profile -> {
+                    String location = String.format("classpath:web-config-%s.yml", profile);
+                    environment.getPropertySources().addLast(parseYamlResource(location));
+                });
+        environment.getPropertySources().addLast(parseYamlResource(("classpath:web-config.yml")));
     }
+
+    /**
+     * parseYamResource
+     * @param location
+     * @return
+     */
+    private PropertySource parseYamlResource(String location) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource(location);
+        if(resource.exists()) {
+            YamlPropertiesFactoryBean yamlPropertiesFactory = new YamlPropertiesFactoryBean();
+            yamlPropertiesFactory.setResources(resource);
+            yamlPropertiesFactory.afterPropertiesSet();
+            return new PropertiesPropertySource(location, yamlPropertiesFactory.getObject());
+        }else{
+            return new PropertiesPropertySource(location, new Properties());
+        }
+    }
+
 }
