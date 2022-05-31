fe9081e5872e594fd28cf7c12a5698896825223a
chomookun <chomookun@gmail.com>
2022-06-01 04:07:24 +0900

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,7 +14,11 @@ pipeline {
                 MAVEN_CREDENTIALS = credentials('MAVEN_CREDENTIALS')
             }
             steps {
-                cleanWs()
+
+                checkout scm
+
+                cleanWs
+
                 sh "./gradlew publish --stacktrace -DmavenPublishUrl=${MAVEN_PUBLISH_URL} -DmavenUsername=${MAVEN_CREDENTIALS_USR} -DmavenPassword=${MAVEN_CREDENTIALS_PSW}"
             }
         }
