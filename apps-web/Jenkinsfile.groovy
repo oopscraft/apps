51338c30ac1c43ea333d7323a7feae6d18e8ed7a
chomookun <chomookun@gmail.com>
2022-06-13 22:59:45 +0900

    .

--- apps-web/Jenkinsfile.groovy
+++ apps-web/Jenkinsfile.groovy
@@ -2,19 +2,30 @@ pipeline {
     agent any
     parameters {
         string(name: 'STAGE', defaultValue: params.STAGE ?:'dev', description: 'stage')
-        string(name: 'DOCKER_PUBLISH_URL', defaultValue: params.MAVEN_PUBLISH_URL ?:'http://___/repository/maven-snapshot/', description: 'Maven publish URL')
+        string(name: 'GRADLE_EXTRA_OPTION', defaultValue: params.GRADLE_EXTRA_OPTION ?:'--init-script init.gradle --stacktrace', description:'gradle extra option')
+        string(name: 'DOCKER_HOST', defaultValue: params.DOCKER_HOST ?:'__.docker.io', description: 'Docker host')
+        string(name: 'DOCKER_IMAGE', defaultValue: params.DOCKER_IMAGE ?:'___', description: 'Docker image repository')
         credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
                 name: 'DOCKER_CREDENTIALS',
                 defaultValue: params.DOCKER_CREDENTIALS ?:'___',
                 description: 'Docker credentials')
-    }
     stages {
         stage('publish') {
             environment {
                 DOCKER_CREDENTIALS = credentials('DOCKER_CREDENTIALS')
             }
             steps {
-                sh "./gradlew build -x test --stacktrace"
+                cleanWs()
+                checkout scm
+                sh "./gradlew :apps-web:build -x test ${GRADLE_EXTRA_OPTION}"
+                sh '''        
+                    # docker builds and push
+                    cd apps-web
+                    echo ${DOCKER_CREDENTIALS_PSW} | sudo docker login --username ${DOCKER_CREDENTIALS_USR} --password-stdin ${DOCKER_HOST}
+                    sudo docker rmi $(sudo docker images ${DOCKER_IMAGE} -q) || true
+                    sudo docker build -t ${DOCKER_HOST}/${DOCKER_IMAGE}:${STAGE} --build-arg BASE_IMAGE=${BASE_IMAGE} .
+                    sudo docker push ${DOCKER_HOST}/${DOCKER_IMAGE}:${STAGE}
+                '''.stripIndent()
             }
         }
     }
