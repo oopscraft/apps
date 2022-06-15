28404b5158f2a706d64433bb67ff869dffe0bf06
chomookun <chomookun@gmail.com>
2022-06-15 23:26:16 +0900

    .

--- apps-web/Jenkinsfile.groovy
+++ apps-web/Jenkinsfile.groovy
@@ -31,16 +31,56 @@ pipeline {
             }
         }
         stage("deploy") {
-            environment {
-                CONTAINER_IMAGE = "${DOCKER_REPOSITORY}:${PROFILE}"
-            }
             steps {
                 sh '''
-                    cd apps-web
-                    kubectl apply -f ./Deployment.yml 
-                    kubectl get pods,services
-                    kubectl port-forward --address 0.0.0.0 service/apps-web 10000:10000 || true
+                    cat <<EOF | kubectl apply -f -
+                        apiVersion: apps/v1
+                        kind: Deployment
+                        metadata:
+                          name: apps-web 
+                        spec:
+                          selector:
+                            matchLabels:
+                              app: apps-web 
+                          replicas: 2
+                          template:
+                            metadata:
+                              labels:
+                                app: apps-web
+                            spec:
+                              containers:
+                              - name: apps-web
+                                #image: 192.168.0.2:9996/apps-web:dev
+                                image: ${DOCKER_HOST}/${DOCKER_REPOSITORY}:${PROFILE}
+                                ports:
+                                - containerPort: 8080
+                                env:
+                                - name: SPRING_PROFILES_ACTIVE
+                                  value: "local"
+                        ---
+                        apiVersion: v1
+                        kind: Service
+                        metadata:
+                          name: apps-web
+                        spec:
+                          type: LoadBalancer 
+                          selector:
+                            app: apps-web
+                          ports:
+                            - protocol: TCP
+                              port: 10000 
+                              targetPort: 8080
+                    EOF
                 '''.stripIndent()
+
+
+
+//                sh '''
+//                    cd apps-web
+//                    kubectl apply -f ./Deployment.yml
+//                    kubectl get pods,services
+//                    kubectl port-forward --address 0.0.0.0 service/apps-web 10000:10000 || true
+//                '''.stripIndent()
            }
         }
     }
