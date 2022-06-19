ec9edabd4c1c91f4028b4322a9bccb0fb60d5890
chomookun <chomookun@gmail.com>
2022-06-20 02:13:57 +0900

    .

--- apps-web/Jenkinsfile.groovy
+++ apps-web/Jenkinsfile.groovy
@@ -77,6 +77,7 @@ pipeline {
                     # print status
                     kubectl get pods,services,ingress
                 '''.stripIndent()
+                sh("kubectl rollout restart deployment/apps-web")
            }
         }
     }
