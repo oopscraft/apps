904752a5c41ae804fcd76dc3ebcce882f51a2ad5
chomookun <chomookun@gmail.com>
2022-07-01 02:26:29 +0900

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,7 @@ pipeline {
     agent any
     parameters {
         string(name: 'GRADLE_EXTRA_OPTION', defaultValue: params.GRADLE_EXTRA_OPTION ?:'--stacktrace', description:'gradle extra option')
+        string(name: 'PROJECT_VERSION', defaultValue: params.PROJECT_VERSION ?:'1.0.0-SNAPSHOT')
         string(name: 'MAVEN_PUBLISH_URL', defaultValue: params.MAVEN_PUBLISH_URL ?:'http://___/repository/maven-snapshot/', description: 'Maven publish URL')
         credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
                 name: 'MAVEN_CREDENTIALS',
@@ -16,7 +17,7 @@ pipeline {
             steps {
                 cleanWs()
                 checkout scm
-                sh "./gradlew publish -PmavenPublishUrl=${MAVEN_PUBLISH_URL} -PmavenUsername=${MAVEN_CREDENTIALS_USR} -PmavenPassword=${MAVEN_CREDENTIALS_PSW} ${GRADLE_EXTRA_OPTION}"
+                sh "./gradlew publish -x test -DprojectVersion=1.0.0-SNAPSHHT -DmavenPublishUrl=${MAVEN_PUBLISH_URL} -DmavenUsername=${MAVEN_CREDENTIALS_USR} -DmavenPassword=${MAVEN_CREDENTIALS_PSW} ${GRADLE_EXTRA_OPTION}"
             }
         }
     }
