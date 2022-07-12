4b47471c41807d621b2ab0ba62b91250853d8751
chomookun <chomookun@gmail.com>
2022-07-13 01:57:11 +0900

    .

--- apps-batch/src/main/java/org/oopscraft/apps/batch/BatchApplication.java
+++ apps-batch/src/main/java/org/oopscraft/apps/batch/BatchApplication.java
@@ -2,8 +2,11 @@ package org.oopscraft.apps.batch;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.oopscraft.apps.batch.context.BatchContext;
 import org.oopscraft.apps.batch.context.BatchContextFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -15,24 +18,34 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProce
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.boot.context.metrics.buffering.StartupTimeline;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.GenericApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Import(BatchConfiguration.class)
 @RequiredArgsConstructor
-public class BatchApplication implements CommandLineRunner {
+public class BatchApplication implements CommandLineRunner, ApplicationContextAware {
 
-    private static BatchContext batchContext;
+    private static ConfigurableApplicationContext applicationContext = null;
 
-    private final JobLauncher jobLauncher;
+    private static BufferingApplicationStartup bufferingApplicationStartup = new BufferingApplicationStartup(1000);
+
+    private static BatchContext batchContext = null;
 
     private final Job job;
+
+    private final JobLauncher jobLauncher;
 
     /**
      * main
@@ -58,19 +71,88 @@ public class BatchApplication implements CommandLineRunner {
                 .run(args);
     }
 
+    /**
+     * setApplicationContext
+     * @param applicationContextInstance applicationContext
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContextInstance) throws BeansException {
+        applicationContext = (ConfigurableApplicationContext) applicationContextInstance;
+    }
 
+    /**
+     * run
+     * @param args program arguments
+     */
     @Override
     public void run(String... args) throws Exception {
+        try {
+            // profiles startup process
+            printStartupProfile();
 
-        // launch
-        JobExecution jobExecution = jobLauncher.run(job, batchContext.getJobParameters());
-        if(jobExecution.getStatus().isUnsuccessful()){
-            String failureExceptions = jobExecution.getFailureExceptions().stream()
-                    .map(e -> e.getMessage())
-                    .collect(Collectors.joining());
-            log.error("failureExceptions:{}", failureExceptions);
-            throw new RuntimeException(jobExecution + failureExceptions);
+            // logging startup context
+            printStartupContext(args);
+
+            // launch
+            JobExecution jobExecution = jobLauncher.run(job, batchContext.getJobParameters());
+            log.info("JobExecution:{}", jobExecution);
+
+            // check isUnsuccessful
+            if(jobExecution.getStatus().isUnsuccessful()){
+                String failureExceptions = jobExecution.getFailureExceptions().stream()
+                        .map(ExceptionUtils::getStackTrace)
+                        .collect(Collectors.joining());
+                throw new RuntimeException(jobExecution + failureExceptions);
+            }
+
+            // check stopped
+            if(jobExecution.getStatus() == BatchStatus.STOPPED){
+                log.warn("== BatchStatus is [{}]", jobExecution.getStatus().name());
+                // SIGTERM(143)
+                System.exit(143);
+            }
+
+        }catch(Exception e){
+            log.error("{}", e.getMessage());
+            throw e;
         }
+    }
+
+    /**
+     * printStartupProfile
+     */
+    private static void printStartupProfile() {
+        log.debug("[START] printStartupProfile");
+        StartupTimeline startupTimeline = bufferingApplicationStartup.getBufferedTimeline();
+        List<StartupTimeline.TimelineEvent> events =  startupTimeline.getEvents();
+        for(StartupTimeline.TimelineEvent event : events){
+            log.debug("- [{}] {} - {}",
+                    String.format("%4s seconds", event.getDuration().getSeconds()),
+                    event.getStartupStep().getName(),
+                    StreamSupport.stream(event.getStartupStep().getTags().spliterator(), false)
+                            .map(tag->String.format("[%s]%s",tag.getKey(),tag.getValue()))
+                            .collect(Collectors.joining(","))
+            );
+        }
+        log.debug("[END] printStartupProfile");
+    }
+
+    /**
+     * printStartupContext
+     * @param args
+     */
+    private static void printStartupContext(String[] args) {
+        log.info(StringUtils.repeat("=", 80));
+        log.info("= BatchApplication.run");
+        log.info("= STAGE:{}", Arrays.toString(applicationContext.getEnvironment().getActiveProfiles()));
+        for(int i = 0; i < args.length; i ++ ) {
+            log.info("= args[{}]:{}", i, args[i]);
+        }
+        log.info("= BatchConfig.getDataHome():{}", BatchConfig.getDataHome());
+        for(String name : batchContext.getJobParameterNames()) {
+            log.info("= batchContext.getJobParameter({}):{}", name, batchContext.getJobParameter(name));
+        }
+        log.info(StringUtils.repeat("=", 80));
     }
 
 }
--- apps-batch/src/test/java/org/oopscraft/apps/batch/BatchApplicationTest.java
+++ apps-batch/src/test/java/org/oopscraft/apps/batch/BatchApplicationTest.java
@@ -1,4 +1,67 @@
 package org.oopscraft.apps.batch;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.internal.util.Assert;
+import org.oopscraft.apps.batch.context.BatchContext;
+import org.oopscraft.apps.batch.job.AbstractJob;
+import org.oopscraft.apps.batch.job.AbstractJobTest;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
 public class BatchApplicationTest {
+
+    @Configuration
+    @ConditionalOnExpression("${org.oopscraft.apps.batch.BatchApplicationTest$TestProxyModeJob:false}")
+    @Slf4j
+    @RequiredArgsConstructor
+    public static class TestProxyModeJob extends AbstractJob {
+
+        @Override
+        public void initialize(BatchContext batchContext) {
+            log.info("== initialize: {}", batchContext);
+            addStep(step());
+        }
+
+        @Bean
+        @JobScope
+        public Step step() {
+            return getStepBuilderFactory().get("testStep01")
+                    .tasklet(tasklet(null))
+                    .build();
+        }
+
+        @Bean
+        @StepScope
+        public Tasklet tasklet(@Value("#{jobParameters[message]}")String message) {
+            return (contribution, chunkContext) -> {
+                log.info("== message: {}", message);
+                Assert.notNull(message);
+                return RepeatStatus.FINISHED;
+            };
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        BatchApplication.main(new String[]{
+                BatchApplicationTest.TestProxyModeJob.class.getName(),
+                "20110101",
+                "message=hello",
+                String.format("uuid=%s", UUID.randomUUID().toString()),
+                "--org.oopscraft.apps.batch.BatchApplicationTest$TestProxyModeJob=true"
+        });
+    }
+
+
 }
