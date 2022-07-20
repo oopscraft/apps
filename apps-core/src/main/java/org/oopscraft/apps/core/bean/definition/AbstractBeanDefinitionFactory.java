c44df804d6b23a294e536e595ec108fad1cbde51
chomookun <chomookun@gmail.com>
2022-07-21 01:42:52 +0900

    .

--- apps-core/src/main/java/org/oopscraft/apps/core/bean/definition/AbstractBeanDefinitionFactory.java
+++ apps-core/src/main/java/org/oopscraft/apps/core/bean/definition/AbstractBeanDefinitionFactory.java
@@ -8,6 +8,9 @@ import java.util.Set;
 
 public abstract class AbstractBeanDefinitionFactory {
 
+    /**
+     * factory registry
+     */
     private static Set<AbstractBeanDefinitionFactory> beaDefinitionFactoryRegistry = new HashSet<AbstractBeanDefinitionFactory>(){{
         add(new ComponentBeanDefinitionFactory());
         add(new MapperBeanDefinitionFactory());
@@ -15,6 +18,12 @@ public abstract class AbstractBeanDefinitionFactory {
         add(new ControllerBeanDefinitionFactory());
     }};
 
+    /**
+     * gets bean definition factory
+     * @param beanClass
+     * @return
+     * @throws UnsupportedBeanTypeException
+     */
     public static AbstractBeanDefinitionFactory getBeanDefinitionFactory(Class<?> beanClass) throws UnsupportedBeanTypeException {
         for(AbstractBeanDefinitionFactory beanDefinitionFactory : beaDefinitionFactoryRegistry){
             if(beanDefinitionFactory.support(beanClass)){
@@ -24,8 +33,18 @@ public abstract class AbstractBeanDefinitionFactory {
         throw new UnsupportedBeanTypeException(beanClass);
     }
 
+    /**
+     * support
+     * @param beanClass
+     * @return
+     */
     public abstract boolean support(Class<?> beanClass);
 
+    /**
+     * gets bean definition
+     * @param beanClass candidate bean class
+     * @return
+     */
     public abstract BeanDefinition getBeanDefinition(Class<?> beanClass);
 
 }
